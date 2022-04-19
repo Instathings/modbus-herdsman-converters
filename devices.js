cc41c537d2ee53773ec7e66d3912c42d83372881
dadna <damianomailswork@gmail.com>
2022-04-19 16:47:32 +0200

    added zcs hyd600 descriptor

--- devices.js
+++ devices.js
@@ -1,9 +1,11 @@
 const xymd = require('./devices/XY-MD02');
 const sdm120 = require('./devices/SDM120');
+const hyd6000 = require('./devices/ZCS_HYD600');
 
 const devices = [
   xymd,
   sdm120,
+  hyd6000
 ];
 
 module.exports = devices;
